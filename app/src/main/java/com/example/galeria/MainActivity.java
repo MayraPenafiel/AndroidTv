7061ec047c24914e84edd83cbcd1c2722ae714cf
cindycedillo <cindy.cedillo.est@tecazuay.edu.ec>
2022-02-01 02:49:50 -0500

    Pantalla inicial añadida

--- app/src/main/java/com/example/galeria/MainActivity.java
+++ app/src/main/java/com/example/galeria/MainActivity.java
@@ -4,22 +4,37 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
     public static int yu;
-    TextView ty;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
-        ty=findViewById(R.id.Tv);
-        ty.setOnClickListener(new View.OnClickListener() {
+
+        Animation animacion1= AnimationUtils.loadAnimation( this,R.anim.desplazamiento_arriba);
+        ImageView logo_galeria= findViewById(R.id.logo_galeria);
+        logo_galeria.setAnimation(animacion1);
+
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, menuPrincipal.class));
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,menuPrincipal.class);
+                startActivity(intent);
+                finish();
+
             }
-        });
+        }, 3000);
+
     }
 }
