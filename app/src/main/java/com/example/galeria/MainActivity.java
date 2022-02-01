fc175367cd799690dab9b65a259875f17cdc4d51
Jey <89442351+MayraPenafiel@users.noreply.github.com>
2022-01-31 19:03:06 -0500

    creacion de la galeria
    cracion de la galeria en la pantalla iniciañ

--- app/src/main/java/com/example/galeria/MainActivity.java
+++ app/src/main/java/com/example/galeria/MainActivity.java
@@ -3,12 +3,20 @@ package com.example.galeria;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.GridView;
+
+import com.example.galeria.adaptadores.GaleriaImagenesAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
+    GridView grid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        grid= findViewById(R.id.grid_view_imagenes);
+        grid.setAdapter(new GaleriaImagenesAdapter(this));
+
     }
 }
